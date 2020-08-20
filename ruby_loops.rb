5c6bf112b96b8c2d98beadf3f33139a5ded7eeee
Joshua Lovelace <rakmial@gmail.com>
2020-08-20 10:51:27 -0700

    demo .each with exp fun

--- ruby_loops.rb
+++ ruby_loops.rb
@@ -1,11 +1,22 @@
 def times_loop(loops_num)
     puts "how many times must I loop you?!"
     loopd_num = 0
-    loops_num.times() do
+    loops_num.times do
         loopd_num += 1
         puts "#{loopd_num} loop!"
     end
     puts "that's #{loopd_num} loops!"
 end
 
-times_loop(5)
+times_loop(10)
+
+def base_2_range_exps_from_neg_to_pos(start, end_)
+    exp_range = start..end_
+    rarr = []
+    exp_range.each do |superscriptoid|
+        rarr.push(2**superscriptoid)
+    end
+    puts rarr
+end
+
+base_2_range_exps_from_neg_to_pos(-10, 10)
